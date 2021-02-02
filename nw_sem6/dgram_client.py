5a43ef8f1885b90ccc61e551d0dacdf70796dae9
mivinromo2011 <mithun.rosinth2011@protonmail.com>
2021-02-02 13:06:38 +0530

    UDP programs for networking

---
+++ nw_sem6/dgram_client.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+udp_host = socket.gethostname()
+udp_port = 12345
+msg ="Hello Python!"
+print("UDP target IP:", udp_host)
+print("UDP target Port:", udp_port)
+sock.sendto(msg.encode(),(udp_host,udp_port))
